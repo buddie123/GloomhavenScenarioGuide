fc5a2b63110ff56fa8f5406243d6cb517debb3e1
Robert Reed <robert.reed@rocketmail.com>
2018-09-29 00:44:15 -0500

    Initial commit. Created the project with an initial activity: ExpressLookup. Closes #5. Closes #1

---
+++ app/src/main/java/robby35us/com/gloomhavenscenarioguide/ExpressLookupFragment.java
@@ -0,0 +1,22 @@
+package robby35us.com.gloomhavenscenarioguide;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ExpressLookupFragment extends Fragment {
+
+    public ExpressLookupFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_express_lookup, container, false);
+    }
+}
