405f257796a4499b2dd5ec9150e3d7ebcc302bc8
Robert Reed <robert.reed@rocketmail.com>
2018-09-29 03:15:45 -0500

    Set the values of the relevant views in ExpressLookupFragment.java. Closes #6.

--- app/src/main/java/robby35us/com/gloomhavenscenarioguide/ExpressLookupFragment.java
+++ app/src/main/java/robby35us/com/gloomhavenscenarioguide/ExpressLookupFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,6 +22,20 @@ public class ExpressLookupFragment extends Fragment {
     public View onCreateView(@NonNull  LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_express_lookup, container, false);
+        TextView selectionTitle = view.findViewById(R.id.selectionTitle);
+        selectionTitle.setText(R.string.label_express_lookup);
+
+        ImageButton navLeftButton = view.findViewById(R.id.navLeftButton);
+        navLeftButton.setVisibility(View.INVISIBLE);
+
+        ImageButton navRightButton = view.findViewById(R.id.navRightButton);
+        navRightButton.setVisibility(View.INVISIBLE);
+
+        final ToggleButton mainToggleButton = view.findViewById(R.id.mainToggleButton);
+
+        final ToggleButton sideToggleButton = view.findViewById(R.id.sideToggleButton);
+
+        final ToggleButton bothToggleButton = view.findViewById(R.id.bothToggleButton);
 
         return view;
     }
